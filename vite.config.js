878803c256ec74dddd4727a08ad24eb83eadfd11
Aubinsossou <164001511+Aubinsossou@users.noreply.github.com>
2026-01-18 00:13:41 +0100

    Correct server proxy configuration formatting
    Fix indentation and formatting in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-server: {
         proxy: {
           '/api': { // Any request starting with /api will be proxied
             target: 'https://quizmania.free.nf', // Replace with your backend server's address
             changeOrigin: true, // Rewrites the origin header to match the target
             rewrite: (path) => path.replace(/^\/api/, ''), // Removes /api from the request path before forwarding
           },
-        },
       },
   plugins: [
     vue(),
