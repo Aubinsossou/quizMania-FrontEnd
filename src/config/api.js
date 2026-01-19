b5dd72ac3ac2a3fa9d85ebabc7285bf5e76fde43
Aubin SOSSOU <aubinsossoua@gmail.com>
2026-01-19 22:19:46 +0100

    changement base url api vers le hostinger business

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
 // Configuration des API
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://quizmania.free.nf/';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://quizmania.projet.sbs/public';
